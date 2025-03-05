6f46e3c69c2e2798ccea24e2463912c5bf56aeb6
Daniel Kim <dehypnosis@gmail.com>
2025-03-05 14:35:36 +0900

    refactor(nxfinance): produce multiple user snapshots (handled by snaoshot loader)

--- src/source/nxfinance-looping.ts
+++ src/source/nxfinance-looping.ts
@@ -1,44 +1,37 @@
 import { AnchorProvider, Program, Wallet, web3 } from '@coral-xyz/anchor';
 import { SourceStreamFactory } from '.';
 import { RPCClient } from '../rpc';
-import NxfinanceIDLFile from './nxfinance.idl.json';
+import nxfinanceIDLFile from './nxfinance.idl.json';
 import { NxLend } from './nxfinance.idl';
 import Decimal from 'decimal.js';
 
+
 export const nxfinanceLooping: SourceStreamFactory = async (opts) => {
   const rpc = new RPCClient(opts.rpc);
-
   const inputToken = new web3.PublicKey(opts.args[0]);
 
   const nxfinanceProgram = new Program(
-    NxfinanceIDLFile as NxLend,
+    nxfinanceIDLFile as NxLend,
     new AnchorProvider(rpc.v1, new Wallet(new web3.Keypair())),
   );
 
-  const positions = await nxfinanceProgram.account.position.all();
-
-  const userCollateralMap = new Map<web3.PublicKey, Decimal>();
+  const positionAccounts = await nxfinanceProgram.account.position.all();
 
   process.nextTick(() => {
     try {
-      for (const position of positions) {
-        const owner = position.account.owner;
-        for (const _position of position.account.positions) {
-          if (_position.collateralMint.equals(inputToken)) {
-            const collateralAmount = userCollateralMap.get(owner) || new Decimal(0);
-            userCollateralMap.set(
-              owner,
-              collateralAmount.add(_position.collateralTokens.toString()),
-            );
+      for (const positionAccount of positionAccounts) {
+        const owner = positionAccount.account.owner;
+        for (const position of positionAccount.account.positions) {
+          if (position.collateralMint.equals(inputToken)) {
+            const amount = new Decimal(position.collateralTokens.toString());
+            if (!amount.isZero()) {
+              opts.produceSnapshot({
+                owner: owner.toString(),
+                baseTokenBalance: amount.toNumber(),
+              });
+            }
           }
         }
-      }
-
-      for (const [owner, collateralAmount] of [...userCollateralMap]) {
-        opts.produceSnapshot({
-          owner: owner.toString(),
-          baseTokenBalance: collateralAmount.toNumber(),
-        });
       }
     } catch (error) {
       opts.close(error as Error);
