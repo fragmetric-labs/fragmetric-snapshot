5d2a27ab81d208b2e40265138ba2cb8bd2ae9d64
Никита Семенчук <n.semenchuk@texture.finance>
2025-05-28 12:51:58 +0300

    feat: add info about borrows exist to snapshot, texture-lending

--- src/commands/snapshot/source/texture-lending.ts
+++ src/commands/snapshot/source/texture-lending.ts
@@ -33,11 +33,14 @@ export const textureLending: SourceStreamFactory = async (opts) => {
             return sum.add(new Decimal(item.deposited_amount.toString()));
           }, new Decimal(0));
 
+        const hasBorrows = position.borrows.length > 0;
+
         if (collateralAmount.isZero()) continue;
 
         opts.produceSnapshot({
           owner: owner.toString(),
           baseTokenBalance: collateralAmount.floor().toNumber(),
+          hasBorrows,
         });
       }
     } catch (error) {
