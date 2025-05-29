a04722899ccd225412088f68ce52faed420ad7a7
Junghyun Chun <ouoabcde@naver.com>
2025-05-29 18:13:26 +0900

    fix: add soon bridge test

--- src/commands/snapshot/source/soon-bridge.test.ts
+++ src/commands/snapshot/source/soon-bridge.test.ts
@@ -7,7 +7,10 @@ describe('snapshot source: soon-bridge', async () => {
     await expectSnapshotSourceWorks(soonBridge, {
       source: 'soon-bridge',
       rpc: SOON_SVM_MAINNET_RPC,
-      args: [],
+      args: [
+        'PMST7CMBeJubWwhieuTvjgyEEwH8FLdNorZdvMJ3aVA',
+        'WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U',
+      ],
     });
   });
 });
