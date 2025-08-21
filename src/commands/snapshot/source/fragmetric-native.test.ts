b9319fd44a61df4b092014512c1bbb9884f8cc2d
taehoon <kth7024@naver.com>
2025-08-21 11:50:29 +0900

    refactor: test code

--- src/commands/snapshot/source/fragmetric-native.test.ts
+++ src/commands/snapshot/source/fragmetric-native.test.ts
@@ -3,40 +3,39 @@ import { fragmetricNative } from './fragmetric-native';
 import { expectSnapshotSourceWorks } from './testutil';
 
 describe('snapshot source: fragmetric-native', async () => {
-  test('fragSOL receipt token balance', async () => {
-    await expectSnapshotSourceWorks(fragmetricNative, {
-      source: 'fragmetric-native',
+  test.each([
+    {
+      name: 'fragSOL receipt token balance',
       args: [
         '4YHmpuyY54Bsj61qNxYGgtQy8xhacfnhdZ6W92rqB64w',
         'FRAGSEthVFL7fdqM8hxfxkfCZzUvmg21cqPJVvC1qdbo',
       ],
-    });
-  });
-  test('fragJTO receipt token balance', async () => {
-    await expectSnapshotSourceWorks(fragmetricNative, {
-      source: 'fragmetric-native',
+    },
+    {
+      name: 'fragJTO receipt token balance',
       args: [
         'EQmDqtcHbTU19twRoRPmjntd9zG3PeeuG4eB8ex3TUio',
         'FRAGJ157KSDfGvBJtCSrsTWUqFnZhrw4aC8N8LqHuoos',
       ],
-    });
-  });
-  test('fragBTC receipt token balance', async () => {
-    await expectSnapshotSourceWorks(fragmetricNative, {
-      source: 'fragmetric-native',
+    },
+    {
+      name: 'fragBTC receipt token balance',
       args: [
         'DbSAJ5oAWrhLLvmwCpNZXggGvLxaUHAqd9vrhDk9p2rf',
         'FRAGB4KZGLMy3wH1nBajP3Q17MHnecEvTPT6wb4pX5MB',
       ],
-    });
-  });
-  test('frag2 receipt token balance', async () => {
-    await expectSnapshotSourceWorks(fragmetricNative, {
-      source: 'fragmetric-native',
+    },
+    {
+      name: 'frag2 receipt token balance',
       args: [
         'CZ92Kdfnaf4WJzw3MDvnSmak9912ejUdSJiQtHX4qUNb',
         'FRAG2gPNXozPpYcn2a8zK7YdtfNXCLsioZNwZXwTQ3cP',
       ],
+    },
+  ])('$name', async ({ args }) => {
+    await expectSnapshotSourceWorks(fragmetricNative, {
+      source: 'fragmetric-native',
+      args,
     });
   });
 });
