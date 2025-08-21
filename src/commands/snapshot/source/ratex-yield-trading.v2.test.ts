248836860146e2c914c8c99add492d0e718fd143
Junghyun Chun <ouoabcde@naver.com>
2025-08-21 13:36:41 +0900

    test: update market addresses

--- src/commands/snapshot/source/ratex-yield-trading.v2.test.ts
+++ src/commands/snapshot/source/ratex-yield-trading.v2.test.ts
@@ -3,8 +3,9 @@ import { ratexV2YieldTrading } from './ratex-yield-trading.v2';
 import { expectSnapshotSourceWorks } from './testutil';
 
 describe('snapshot source: ratex-v2-yield-trading', async () => {
-  // https://app.rate-x.io/points?symbol=fragSOL-2509
-  test('fragSOL-2508 market', async () => {
+  // staging
+  // https://app-uat.rate-x.io/points?symbol=fragSOL-2509
+  test('fragSOL-2509 market', async () => {
     await expectSnapshotSourceWorks(ratexV2YieldTrading, {
       source: 'ratex-yield-trading',
       args: [
@@ -14,14 +15,73 @@ describe('snapshot source: ratex-v2-yield-trading', async () => {
     });
   });
 
-  // https://app.rate-x.io/points?symbol=fragJTO-2508
-  // test('fragJTO-2508 market', async () => {
-  //   await expectSnapshotSourceWorks(ratexV2YieldTrading, {
-  //     source: 'ratex-v2-yield-trading',
-  //     args: [
-  //       '3QKqmp93duR1mAFmHt33QYJ7eGU9sXPUQrDeX3zGrF2b',
-  //       'WFRGJnQt5pK8Dv4cDAbrSsgPcmboysrmX3RYhmRRyTR',
-  //     ],
-  //   });
-  // });
+  // production
+  // fragSOL
+  // https://app.rate-x.io/points?symbol=fragSOL-2510
+  test('fragSOL-2510 market', async () => {
+    await expectSnapshotSourceWorks(ratexV2YieldTrading, {
+      source: 'ratex-yield-trading',
+      args: [
+        '3jB3LKEy6xR1LCWArzqHsZ9v5CWxNNFhNzqy51xv5gEw',
+        'WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U',
+      ],
+    });
+  });
+
+  // https://app.rate-x.io/points?symbol=fragSOL-2601
+  test('fragSOL-2601 market', async () => {
+    await expectSnapshotSourceWorks(ratexV2YieldTrading, {
+      source: 'ratex-yield-trading',
+      args: [
+        'HrrW6M4EsJjkEF2eMXXM9Ash5iNy7ino2ZUxbxBGCXTM',
+        'WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U',
+      ],
+    });
+  });
+
+  // fragJTO
+  // https://app.rate-x.io/points?symbol=fragJTO-2510
+  test('fragJTO-2510 market', async () => {
+    await expectSnapshotSourceWorks(ratexV2YieldTrading, {
+      source: 'ratex-yield-trading',
+      args: [
+        'Cthq9BbpctLMAZw7DyWmvVcX9uyuSoVeCNBcepSjSmmL',
+        'WFRGJnQt5pK8Dv4cDAbrSsgPcmboysrmX3RYhmRRyTR',
+      ],
+    });
+  });
+
+  // https://app.rate-x.io/points?symbol=fragJTO-2601
+  test('fragJTO-2601 market', async () => {
+    await expectSnapshotSourceWorks(ratexV2YieldTrading, {
+      source: 'ratex-yield-trading',
+      args: [
+        '4JsDhy51uY8tUfWSJ4gUFz4apo7ua5FwjZgZDEkxTof8',
+        'WFRGJnQt5pK8Dv4cDAbrSsgPcmboysrmX3RYhmRRyTR',
+      ],
+    });
+  });
+
+  // fragBTC
+  // https://app.rate-x.io/points?symbol=fragBTC-2510
+  test('fragBTC-2510 market', async () => {
+    await expectSnapshotSourceWorks(ratexV2YieldTrading, {
+      source: 'ratex-yield-trading',
+      args: [
+        'GEkVuB7mPNNCdHwYwo9iLr3RAYwc914HTPG5YVN2NXkQ',
+        'WFRGB49tP8CdKubqCdt5Spo2BdGS4BpgoinNER5TYUm',
+      ],
+    });
+  });
+
+  // https://app.rate-x.io/points?symbol=fragBTC-2601
+  test('fragBTC-2601 market', async () => {
+    await expectSnapshotSourceWorks(ratexV2YieldTrading, {
+      source: 'ratex-yield-trading',
+      args: [
+        'DWNtGEMjzCoS6TTEKniA7tDyGBnb8oBDbBR7Qq8AVFYL',
+        'WFRGB49tP8CdKubqCdt5Spo2BdGS4BpgoinNER5TYUm',
+      ],
+    });
+  });
 });
