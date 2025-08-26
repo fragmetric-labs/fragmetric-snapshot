f28779d43cfb870244a3644069a2aa90ff7ec5a8
Daniel Kim <dehypnosis@gmail.com>
2025-08-26 18:00:16 +0900

    test(kamono-lending): tested with wfragSOL reserve

--- src/commands/snapshot/source/kamino-lending.test.ts
+++ src/commands/snapshot/source/kamino-lending.test.ts
@@ -1,4 +1,16 @@
 import { describe, test, expect } from 'vitest';
-// import { kaminoLending } from './kamino-lending';
+import { expectSnapshotSourceWorks } from './testutil';
+import { kaminoLending } from './kamino-lending';
 
-describe.skip('snapshot source: kamino-lending', async () => {});
+describe('snapshot source: kamino-lending', async () => {
+  // https://app.kamino.finance/lending/reserve/F4Pn9mAvbUazDmWET5yYATTiyLHLaCRTWgGex4tiMXAs/3w38SEhBoK2LRjuSL8p7cGAZ9hoFZ5gXjpza432HbeKx
+  test('wfragBTC-zBTC liquidity pool', async () => {
+    await expectSnapshotSourceWorks(kaminoLending, {
+      source: 'kamino-lending',
+      args: [
+        '3w38SEhBoK2LRjuSL8p7cGAZ9hoFZ5gXjpza432HbeKx',
+        'WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U',
+      ],
+    });
+  });
+});
