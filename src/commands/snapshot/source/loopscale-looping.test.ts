a40ab4d9e4d5b355d8cd6e87e1eb791f64423e5a
Terry Kang <maple19out@naver.com>
2025-09-02 16:22:50 +0900

    test: add lp token looping tests

--- src/commands/snapshot/source/loopscale-looping.test.ts
+++ src/commands/snapshot/source/loopscale-looping.test.ts
@@ -2,13 +2,13 @@ import { describe, test } from 'vitest';
 import { expectSnapshotSourceWorks } from './testutil';
 import { loopscaleLooping } from './loopscale-looping';
 
-describe('snapshot source: loopscale-looping', async () => {
+describe('snapshot source: loopscale-looping', () => {
   // https://app.loopscale.com/loops/wfragsol-sol
   test('wfragSOL looping', async () => {
     await expectSnapshotSourceWorks(loopscaleLooping, {
       source: 'loopscale-looping',
       args: [
-        '7n5F6vLutwTPuVju9t4ZC22vHyJNyGbHKzaokdyWycjy',
+        'loopscaleLoopingWfragsol11111111111111111111',
         'WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U',
       ],
     });
@@ -19,7 +19,29 @@ describe('snapshot source: loopscale-looping', async () => {
     await expectSnapshotSourceWorks(loopscaleLooping, {
       source: 'loopscale-looping',
       args: [
-        'HgmkjvzLaGa8J7QAmYsQL5b5fgAExGtpYpFxz9LYoHmT',
+        'loopscaleLoopingWfragbtc11111111111111111111',
+        'WFRGB49tP8CdKubqCdt5Spo2BdGS4BpgoinNER5TYUm',
+      ],
+    });
+  });
+
+  // https://app.loopscale.com/loops/orcaFragSolJitoSol-fragSol
+  test.only('OWP wfragSOL-JitoSOL looping', async () => {
+    await expectSnapshotSourceWorks(loopscaleLooping, {
+      source: 'loopscale-looping',
+      args: [
+        'loopscaleLoopingWfragsol11111111111111111OWP',
+        'WFRGSWjaz8tbAxsJitmbfRuFV2mSNwy7BMWcCwaA28U',
+      ],
+    });
+  });
+
+  // https://app.loopscale.com/loops/orcazBTCwfragBTC
+  test.only('OWP wfragBTC-zBTC looping', async () => {
+    await expectSnapshotSourceWorks(loopscaleLooping, {
+      source: 'loopscale-looping',
+      args: [
+        'loopscaleLoopingWfragbtc11111111111111111OWP',
         'WFRGB49tP8CdKubqCdt5Spo2BdGS4BpgoinNER5TYUm',
       ],
     });
